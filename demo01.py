8af3ab273ddd2668937d8c5022f8ba7ba3af0ed5
your name <1184858934@qq.com>
2020-03-24 21:20:15 +0800

    第三次

---
+++ demo01.py
@@ -0,0 +1,48 @@
+"""print("hello world")
+print(2333)  #字符串
+print(2.333)  #小数
+print(True)    #布尔值
+print(())     #元组
+print([])     #字典
+print("你好")  
+print("哈哈"+"嘻嘻")
+锄禾日当午，汗滴禾下土
+print("哈哈",2334)
+print("呵呵"*20)
+print(((1+2)*100-99)/2)
+print(2>3)
+"""
+
+#数据格式的转换
+
+# print(type(""))
+# print(type(1343))
+# print(type(1.22))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a = "哈哈哈"
+# print(len(a))
+
+# a = input("请输入：")
+# b = input("请输入：")
+# print("长度和为",len(a+b))
+
+
+a = (1,2,3,4,"四","五","六","七","八","九","十",True,False)
+# print(a[3])
+# print(a)
+# print(a.index("哈哈"))
+# print(a.index("嘿嘿"))
+# print(a.count("哈哈"))
+# b = (a,"哈哈","嘻嘻")
+# print(b[0])
+# print(b[0][3])
+# print(b[0][5])
+# print(b[0][5],b[1])
+print(a[-2])
+print(a[:4])  #左闭右开原则
+print(a[3:5])
+print(a[9:])
